9212be739c2a81ac5de857e4e91010279d9c9d3f
mstonya2023 <latonya.davis08@gmail.com>
2023-09-26 08:42:29 -0700

    making updates to part 1 of finchcollector

--- main_app/models.py
+++ main_app/models.py
@@ -5,4 +5,5 @@ class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
-    age = models.IntegerField()
+    age = models.IntegerField
+    
