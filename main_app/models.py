3b5b1a82238e52a0cd596863648a3916e7bc254f
mstonya2023 <latonya.davis08@gmail.com>
2023-09-27 15:53:08 -0700

    Add objects in shell

--- main_app/models.py
+++ main_app/models.py
@@ -5,5 +5,7 @@ class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
-    age = models.IntegerField
-    
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'    
