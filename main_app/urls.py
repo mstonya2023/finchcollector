b29dfbbb3b3a3610e9f47e8f2059ab62233ef32f
mstonya2023 <latonya.davis08@gmail.com>
2023-10-02 00:31:21 -0700

    Complete parts 4 and 5

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,11 +4,19 @@ from . import views
 	
  
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
-  path('finch/', views.finch_index, name='index'),
-  path('finch/<int:finch_id>/', views.finch_detail, name='detail'),
-  path('finch/create/', views.FinchCreate.as_view(), name='finch_create'),
-  path('finch/<int:pk>/update/', views.FinchUpdate.as_view(),name='finch_update'),
-  path('finch/<int:pk>/delete/', views.FinchDelete.as_view(),name='finch_delete'),
+path('', views.home, name='home'),
+path('about/', views.about, name='about'),
+path('finch/', views.finch_index, name='index'),
+path('finch/<int:finch_id>/', views.finch_detail, name='detail'),
+path('finch/create/', views.FinchCreate.as_view(), name='finch_create'),
+path('finch/<int:pk>/update/', views.FinchUpdate.as_view(),name='finch_update'),
+path('finch/<int:pk>/delete/', views.FinchDelete.as_view(),name='finch_delete'),
+path('finch/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+path('finch/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+path('finch/<int:finch_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy'),
+path('toys/', views.ToyList.as_view(), name='toys_index'),
+path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
 ]
